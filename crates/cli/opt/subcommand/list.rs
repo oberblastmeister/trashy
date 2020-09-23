4b9f8367e8e4c625185261fda8af8552aca7a9b6
Brian Shu <littlebubu.shu@gmail.com>
2020-09-23 12:11:25 -0400

    added metadata

--- crates/cli/opt/subcommand/list.rs
+++ crates/cli/opt/subcommand/list.rs
@@ -1,18 +1,20 @@
+use std::cmp::Ordering;
+
 use chrono::naive::NaiveDateTime;
 use eyre::{eyre, Result, WrapErr};
 use itertools::Itertools;
 use lazy_static::lazy_static;
 use log::{debug, error, info, warn};
 use lscolors::{LsColors, Style};
-use prettytable::{Cell, Row, Table, row, cell};
+use prettytable::{cell, row, Cell, Row, Table};
 use structopt::StructOpt;
 
-use trash_lib::trash_entry::{self, read_dir_trash_entries};
+use trash_lib::trash_entry::{self, read_dir_trash_entries, TrashEntry};
 use trash_lib::trash_info::TrashInfo;
 use trash_lib::{ok_log, TRASH_FILE_DIR, TRASH_INFO_DIR};
 
 lazy_static! {
-    static ref LS_COLORS: LsColors = LsColors::from_env().unwrap_or_default(); 
+    static ref LS_COLORS: LsColors = LsColors::from_env().unwrap_or_default();
 }
 
 #[derive(StructOpt, Debug, PartialEq)]
@@ -30,16 +32,15 @@ pub fn list(_opt: Opt) -> Result<()> {
     let mut table = Table::new();
     table.add_row(header_row());
 
-    iter
-    .map(|trash_entry| {
-        let trash_info = trash_entry.parse_trash_info();
-        trash_info
-    })
+    iter.map(map_trash_entry)
+    // iter.map(|trash_entry| {
+    //     let trash_info = trash_entry.parse_trash_info();
+    //     trash_info
+    // })
     .filter_map(|res| ok_log!(res => error!))
-    .sorted()
-    .map(|trash_info| -> Result<Row> {
-        row_from_trash_info(trash_info)
-    })
+    // .sorted()
+    .sorted_by(|t1, t2| custom_cmp(t1, t2))
+    .map(|trash_info| row_from_trash_info(trash_info))
     .filter_map(|res| ok_log!(res => error!))
     .for_each(|row| {
         table.add_row(row);
@@ -47,6 +48,17 @@ pub fn list(_opt: Opt) -> Result<()> {
 
     table.printstd();
     Ok(())
+}
+
+fn map_trash_entry(trash_entry: TrashEntry) -> Result<(TrashEntry, TrashInfo)> {
+    let trash_info = trash_entry.parse_trash_info();
+    trash_info
+        .map(|trash_info| (trash_entry, trash_info))
+        .map_err(Into::into)
+}
+
+fn custom_cmp(t1: &(TrashEntry, TrashInfo), t2: &(TrashEntry, TrashInfo)) -> Ordering {
+    t1.1.cmp(&t2.1)
 }
 
 fn row_from_trash_info(trash_info: TrashInfo) -> Result<Row> {
