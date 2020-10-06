0b5bded2ad620283658acf8c2f7ceced6ae9804d
Brian Shu <littlebubu.shu@gmail.com>
2020-10-06 14:18:02 -0400

    started rewriting parser

--- crates/lib/src/parser/parser.rs
+++ crates/lib/src/parser/parser.rs
@@ -2,60 +2,108 @@ use std::borrow::Cow;
 use std::convert::TryInto;
 
 use chrono::NaiveDateTime;
-use nom::bytes::complete::{is_not, tag};
-use nom::character::complete::char;
-use nom::combinator::{all_consuming, map_res};
-use nom::error::{context, VerboseError};
-use nom::sequence::delimited;
-use nom::IResult;
+// use nom::bytes::complete::{is_not, tag};
+// use nom::character::complete::char;
+// use nom::combinator::{all_consuming, map_res};
+// use nom::error::{context, VerboseError};
+// use nom::sequence::delimited;
+// use nom::IResult;
 use percent_encoding::percent_decode_str;
-use snafu::ResultExt;
+use snafu::{ResultExt, Snafu};
 
-use super::error::{Error, ParseNaiveDate};
+use super::error::ParseNaiveDate;
 use super::error::{NomError, Result};
-use crate::trash_info::TrashInfo;
 use crate::percent_path::PercentPath;
+use crate::trash_info::TrashInfo;
 
 pub const TRASH_DATETIME_FORMAT: &'static str = "%Y-%m-%dT%H:%M:%S";
 
-fn parse_line<'a>(
-    i: &'a str,
-    key: &'static str,
-) -> IResult<&'a str, &'a str, VerboseError<&'a str>> {
-    let (i, _) = context(key, tag(key))(i)?;
-    let (i, _) = context("equal sign", char('='))(i)?;
-    is_not("\n")(i)
+// fn parse_line<'a>(
+//     i: &'a str,
+//     key: &'static str,
+// ) -> IResult<&'a str, &'a str, VerboseError<&'a str>> {
+//     let (i, _) = context(key, tag(key))(i)?;
+//     let (i, _) = context("equal sign", char('='))(i)?;
+//     is_not("\n")(i)
+// }
+
+// fn parse_path_line<'a>(i: &'a str) -> IResult<&'a str, &'a str, VerboseError<&'a str>> {
+//     parse_line(i, "Path")
+// }
+
+// fn parse_deletion_date_line<'a>(i: &'a str) -> IResult<&'a str, &'a str, VerboseError<&'a str>> {
+//     parse_line(i, "DeletionDate")
+// }
+
+// fn parse_header_line<'a>(i: &'a str) -> IResult<&'a str, &'a str, VerboseError<&'a str>> {
+//     context("header", delimited(char('['), tag("Trash Info"), char(']')))(i)
+// }
+
+// fn parse_trash_info_str<'a>(i: &'a str) -> IResult<&str, TrashInfoStr, VerboseError<&'a str>> {
+//     let (i, _) = parse_header_line(i)?;
+//     let (i, _) = char('\n')(i)?;
+//     let (i, path) = context(
+//         "percent str",
+//         map_res(parse_path_line, |s| percent_decode_str(s).decode_utf8()),
+//     )(i)?;
+//     let (i, _) = char('\n')(i)?;
+//     let (i, deletion_date) = all_consuming(context("date", parse_deletion_date_line))(i)?;
+
+//     Ok((
+//         i,
+//         TrashInfoStr {
+//             path,
+//             deletion_date,
+//         },
+//     ))
+// }
+
+fn parse_header(i: &str) -> ParseError<&str, &str> {
+    tag("[Trash Info]")(i)
 }
 
-fn parse_path_line<'a>(i: &'a str) -> IResult<&'a str, &'a str, VerboseError<&'a str>> {
-    parse_line(i, "Path")
+type ParseError<I, O, E = Error> = Result<(I, O), E>;
+
+fn tag(tag: &str) -> impl Fn(&str) -> ParseError<&str, &str> + '_ {
+    move |i| {
+        if i.starts_with(tag) {
+            let idx = tag.len();
+            Ok((&i[idx..], &i[..idx]))
+        } else {
+            Tag { i }.fail()
+        }
+    }
 }
 
-fn parse_deletion_date_line<'a>(i: &'a str) -> IResult<&'a str, &'a str, VerboseError<&'a str>> {
-    parse_line(i, "DeletionDate")
+fn char(c: char) -> impl Fn(&str) -> ParseError<&str, char> {
+    move |i| {
+        if i.starts_with(c) {
+            Ok((&i[1..], c))
+        } else {
+            Tag { i }.fail()
+        }
+    }
 }
 
-fn parse_header_line<'a>(i: &'a str) -> IResult<&'a str, &'a str, VerboseError<&'a str>> {
-    context("header", delimited(char('['), tag("Trash Info"), char(']')))(i)
+fn all_consuming<O, F>(f: F) -> impl Fn(&str) -> ParseError<&str, O>
+where
+    F: Fn(&str) -> ParseError<&str, O>,
+{
+    move |i| {
+        let (i, o) = f(i)?;
+        if i.len() == 0 {
+            Ok((i, o))
+        } else {
+            Eof { i }.fail()
+        }
+    }
 }
 
-fn parse_trash_info_str<'a>(i: &'a str) -> IResult<&str, TrashInfoStr, VerboseError<&'a str>> {
-    let (i, _) = parse_header_line(i)?;
-    let (i, _) = char('\n')(i)?;
-    let (i, path) = context(
-        "percent str",
-        map_res(parse_path_line, |s| percent_decode_str(s).decode_utf8()),
-    )(i)?;
-    let (i, _) = char('\n')(i)?;
-    let (i, deletion_date) = all_consuming(context("date", parse_deletion_date_line))(i)?;
-
-    Ok((
-        i,
-        TrashInfoStr {
-            path,
-            deletion_date,
-        },
-    ))
+#[derive(Debug, Snafu)]
+pub enum Error {
+    Tag { i: String },
+    Char { i: String },
+    Eof { i: String },
 }
 
 pub fn parse_trash_info<'a>(i: &'a str) -> Result<TrashInfo> {
@@ -98,14 +146,15 @@ mod tests {
                 eprintln!("{}", e);
                 panic!("An error occurred");
             }
-            Ok(trash_info) => trash_info
+            Ok(trash_info) => trash_info,
         };
 
         let actual = TrashInfo::new(
             PercentPath::from_str(actual.0),
-            Some(
-                NaiveDateTime::parse_from_str(actual.1, TRASH_DATETIME_FORMAT)?
-            ),
+            Some(NaiveDateTime::parse_from_str(
+                actual.1,
+                TRASH_DATETIME_FORMAT,
+            )?),
         );
 
         assert_eq!(expected, actual);
