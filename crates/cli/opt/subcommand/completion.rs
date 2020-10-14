930a52622b9f367c49b982bf182f64884110ad77
Brian Shu <littlebubu.shu@gmail.com>
2020-10-14 10:54:31 -0400

    added arg_enum for shell

--- crates/cli/opt/subcommand/completion.rs
+++ crates/cli/opt/subcommand/completion.rs
@@ -1,25 +1,34 @@
 use std::io;
 
-use clap::{Clap, IntoApp};
+use clap::{ArgEnum, Clap, IntoApp};
 use clap_generate::generate;
 use clap_generate::generators::{Bash, Elvish, Fish, PowerShell, Zsh};
+
+#[derive(Debug, Copy, Clone, ArgEnum)]
+pub enum Shell {
+    Bash,
+    Elvish,
+    Fish,
+    Powershell,
+    Zsh,
+}
 
 #[derive(Debug, Clap)]
 pub struct Opt {
     /// shell to generate copmletions for
-    #[clap(possible_values = &["bash", "elvish", "fish", "powershell", "zsh"])]
-    pub shell: String,
+    #[clap(arg_enum)]
+    #[clap(case_insensitive = true)]
+    pub shell: Shell,
 }
 
 pub fn completion(opt: Opt) {
-    // crate::Opt::clap()
     let mut app = crate::Opt::into_app();
 
-    match &*opt.shell {
-        "bash" => generate::<Bash, _>(&mut app, "trash", &mut io::stdout()),
-        "fish" => generate::<Fish, _>(&mut app, "trash", &mut io::stdout()),
-        "powershell" => generate::<PowerShell, _>(&mut app, "trash", &mut io::stdout()),
-        "zsh" => generate::<Zsh, _>(&mut app, "trash", &mut io::stdout()),
-        _ => panic!("wrong shell"),
+    match opt.shell {
+        Shell::Bash => generate::<Bash, _>(&mut app, "trash", &mut io::stdout()),
+        Shell::Fish => generate::<Fish, _>(&mut app, "trash", &mut io::stdout()),
+        Shell::Powershell => generate::<PowerShell, _>(&mut app, "trash", &mut io::stdout()),
+        Shell::Zsh => generate::<Zsh, _>(&mut app, "trash", &mut io::stdout()),
+        Shell::Elvish => generate::<Elvish, _>(&mut app, "trash", &mut io::stdout()),
     }
 }
